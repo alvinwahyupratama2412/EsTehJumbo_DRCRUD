e6de26f03712710a4926b3c744699776bc1c0404
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2026-04-09 15:14:24 +0700

    Membuat kode untuk menampilkan data NIM, Nama, JenisKelamin, TanggalLahir, TanggalLahir, Alamat, dan Kode prodi Menggunakan SqlDataReader pada event btnLoad_Click

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Linq.Expressions;
 
 
 namespace CRUDMahasiswaADO
@@ -43,6 +44,51 @@ namespace CRUDMahasiswaADO
         private void btnConnect_Click(object sender, EventArgs e)
         {
             ConnectDataBase();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                dataGridView1.Rows.Add("NIM", "NIM");
+                dataGridView1.Rows.Add("Nama", "Nama");
+                dataGridView1.Rows.Add("JenisKelamin", "Jenis Kelamnin");
+                dataGridView1.Rows.Add("TanggalLahir", "Tanggal Lahir");
+                dataGridView1.Rows.Add("Alamat", "Alamat");
+                dataGridView1.Rows.Add("KodeProdi", "Kode Prodi");
+
+                string query = "SELECT * FROM Mahasiswa";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(
+                        reader["NIM"].ToString(),
+                        reader["Nama"].ToString(),
+                        reader["JenisKelamin"].ToString(),
+                        Convert.ToDateTime(reader["TanggalLahir"]).ToShortDateString(),
+                        reader["Alamat"].ToString(),
+                        reader["KodeProdi"].ToString()
+                        );
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal menampilkan data : " + ex.Message);
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
